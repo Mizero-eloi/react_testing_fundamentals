fc205e195acb51fe050cc24bd4b382f8382bf589
Mizero-eloi <80544579+Mizero-eloi@users.noreply.github.com>
2022-09-29 09:32:10 +0200

    initial commits

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test("renders title", () => {
+  render(<App />);
+  const title = screen.getByTestId("mytestid");
+  expect(title).toBeInTheDocument();
+});
+
+test("renders sum", () => {
+  render(<App />);
+  const sum = screen.getByTitle("sum");
+  expect(sum.textContent).toEqual("11");
+});
+
+test("renders 3 lists", () => {
+  render(<App />);
+  const listElements = screen.getAllByRole("listitem");
+  expect(listElements.length).toEqual(3);
 });
